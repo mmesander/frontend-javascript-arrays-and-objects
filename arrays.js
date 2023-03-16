0ea21a8b6530b5bbb513bd23cbd2a9321b073142
mmesander <mark_mesander@hotmail.com>
2023-03-16 20:35:00 +0100

    Opdracht array's gemaakt

--- arrays.js
+++ arrays.js
@@ -2,17 +2,23 @@
 // 1. Declareer een variabele en sla daar een array met vier jaartallen (zoals 2021) in op. Log dit in de terminal.
 // ==========================================
 
+years = [2020, 2021, 2022]
+console.log(years)
 
 // ==========================================
 // 2. Declareer een variabele en sla daar een array met drie van jouw hobbies (zoals muziek luisteren en koekjes eten) in op. Log dit in de terminal.
 // ==========================================
 
+hobbys = ["Gamen", "Sleutelen", "Sporten", "Koken"]
+console.log(hobbys)
 
 // ==========================================
 // 3a. Declareer een variabele met daarin een array met de waardes 3, 4, 5, 7 en 2
 // 3b. Zorg er nu voor dat je de eerste waarde uit die array in de terminal logt             // geeft 3
 // ==========================================
 
+values = [3, 4, 5, 7, 2]
+console.log(values[0])
 
 // ==========================================
 // 4a. Declareer een variabele met daarin een array met de waardes groen, geel, rood, paars, blauw en oranje.
@@ -21,6 +27,9 @@
 // 4c. Zorg ervoor dat je de tweede waarde uit bovenstaande array in de terminal logt       // geeft geel
 // ==========================================
 
+colors = ["groen", "geel", "rood", "paars", "blauw", "oranje"]
+console.log(colors[4])
+console.log(colors[1])
 
 // ==========================================
 // 5a. Declareer een variabele met daarin een array met de waardes 21, 22, 23, 25, 25 en log dit in de terminal
@@ -28,6 +37,9 @@
 // 5c. Log de array in de terminal                                                          // geeft [ 21, 22, 23, 24, 25 ]
 // ==========================================
 
+values2 = [21, 22, 23, 25, 25]
+values2[3] = 24
+console.log(values2)
 
 // ==========================================
 // 6a. Declareer een variabele met daarin een array met de waardes bladerdeeg, knoflook, spinazie
@@ -35,3 +47,8 @@
 // 6c. Log de de array in de terminal                                                       // geeft [ 'lasagne bladen', 'knoflook', 'spinazie' ]
 // 6d. Log de lengte van de array in de terminal                                            // geeft 3
 // ==========================================
+
+ingredients = ["bladerdeeg", "knoflook", "spinazie"]
+ingredients[0] = "lasagna bladen"
+console.log(ingredients)
+console.log(ingredients.length)
