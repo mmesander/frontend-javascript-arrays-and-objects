0266b29bfd5bbf8d46e3b4f623b90e72fa990f01
mmesander <mark_mesander@hotmail.com>
2023-03-16 20:36:31 +0100

    const erbij toegevoegd

--- arrays.js
+++ arrays.js
@@ -2,14 +2,14 @@
 // 1. Declareer een variabele en sla daar een array met vier jaartallen (zoals 2021) in op. Log dit in de terminal.
 // ==========================================
 
-years = [2020, 2021, 2022]
+const years = [2020, 2021, 2022]
 console.log(years)
 
 // ==========================================
 // 2. Declareer een variabele en sla daar een array met drie van jouw hobbies (zoals muziek luisteren en koekjes eten) in op. Log dit in de terminal.
 // ==========================================
 
-hobbys = ["Gamen", "Sleutelen", "Sporten", "Koken"]
+const hobbys = ["Gamen", "Sleutelen", "Sporten", "Koken"]
 console.log(hobbys)
 
 // ==========================================
@@ -17,7 +17,7 @@ console.log(hobbys)
 // 3b. Zorg er nu voor dat je de eerste waarde uit die array in de terminal logt             // geeft 3
 // ==========================================
 
-values = [3, 4, 5, 7, 2]
+const values = [3, 4, 5, 7, 2]
 console.log(values[0])
 
 // ==========================================
@@ -27,7 +27,7 @@ console.log(values[0])
 // 4c. Zorg ervoor dat je de tweede waarde uit bovenstaande array in de terminal logt       // geeft geel
 // ==========================================
 
-colors = ["groen", "geel", "rood", "paars", "blauw", "oranje"]
+const colors = ["groen", "geel", "rood", "paars", "blauw", "oranje"]
 console.log(colors[4])
 console.log(colors[1])
 
@@ -37,7 +37,7 @@ console.log(colors[1])
 // 5c. Log de array in de terminal                                                          // geeft [ 21, 22, 23, 24, 25 ]
 // ==========================================
 
-values2 = [21, 22, 23, 25, 25]
+const values2 = [21, 22, 23, 25, 25]
 values2[3] = 24
 console.log(values2)
 
@@ -48,7 +48,7 @@ console.log(values2)
 // 6d. Log de lengte van de array in de terminal                                            // geeft 3
 // ==========================================
 
-ingredients = ["bladerdeeg", "knoflook", "spinazie"]
+const ingredients = ["bladerdeeg", "knoflook", "spinazie"]
 ingredients[0] = "lasagna bladen"
 console.log(ingredients)
 console.log(ingredients.length)
